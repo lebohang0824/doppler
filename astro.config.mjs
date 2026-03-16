c343d7137dc444578ae0a41bc08fe67094eb5b66
Lebohang Mokoena <lebohang0824@gmail.com>
2026-03-16 18:13:44 +0200

    Added packages: - DB - Alpine Js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import alpinejs from '@astrojs/alpinejs';
+
+import db from '@astrojs/db';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [alpinejs(), db()]
+});
